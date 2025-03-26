0aef6fa4a169f295f1b96af65d0223a425c39d64
sherdoslav <seryji2010cz@gmail.com>
2025-03-26 15:49:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
 // on A button press
 input.onButtonPressed(Button.A, function () {
-    if (can_start == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        led.plot(point_x, point_y)
-        can_start = 2
-    } else if (can_start == 2) {
-        led.unplot(point_x, point_y)
-        if (point_y != 4) {
-            point_y += 1
-        } else {
-            point_y = 0
+    if (wall_x != point_x) {
+        if (can_start == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            led.plot(point_x, point_y)
+            can_start = 2
+        } else if (can_start == 2) {
+            led.unplot(point_x, point_y)
+            if (point_y != 4) {
+                point_y += 1
+            } else {
+                point_y = 0
+            }
+            led.plot(point_x, point_y)
         }
-        led.plot(point_x, point_y)
     }
 })
 // on B button press
@@ -25,11 +27,13 @@ input.onButtonPressed(Button.B, function () {
 	
 })
 let can_start = 0
+let wall_x = 0
 let point_y = 0
 let point_x = 0
 point_x = 1
 point_y = 2
-let wall_x = 4
+wall_x = 4
+let wall_brightness = 100
 basic.showLeds(`
     . . . . .
     . # . # .
@@ -47,18 +51,15 @@ basic.showLeds(`
     `)
 can_start = 1
 let wall_status = 1
-loops.everyInterval(500, function () {
-	
-})
 basic.forever(function () {
     basic.pause(500)
     if (can_start == 2) {
         if (wall_status == 1) {
-            led.plot(wall_x, 0)
-            led.plot(wall_x, 1)
-            led.plot(wall_x, 2)
-            led.plot(wall_x, 3)
-            led.plot(wall_x, 4)
+            led.plotBrightness(wall_x, 0, wall_brightness)
+            led.plotBrightness(wall_x, 1, wall_brightness)
+            led.plotBrightness(wall_x, 2, wall_brightness)
+            led.plotBrightness(wall_x, 3, wall_brightness)
+            led.plotBrightness(wall_x, 4, wall_brightness)
             wall_status = 2
         } else if (wall_status == 2) {
             if (wall_x != 0) {
@@ -68,11 +69,11 @@ basic.forever(function () {
                 led.unplot(wall_x, 3)
                 led.unplot(wall_x, 4)
                 wall_x += -1
-                led.plot(wall_x, 0)
-                led.plot(wall_x, 1)
-                led.plot(wall_x, 2)
-                led.plot(wall_x, 3)
-                led.plot(wall_x, 4)
+                led.plotBrightness(wall_x, 0, wall_brightness)
+                led.plotBrightness(wall_x, 1, wall_brightness)
+                led.plotBrightness(wall_x, 2, wall_brightness)
+                led.plotBrightness(wall_x, 3, wall_brightness)
+                led.plotBrightness(wall_x, 4, wall_brightness)
             } else {
                 led.unplot(wall_x, 0)
                 led.unplot(wall_x, 1)
@@ -81,11 +82,14 @@ basic.forever(function () {
                 led.unplot(wall_x, 4)
                 wall_x = 4
                 basic.pause(200)
-                led.plot(wall_x, 0)
-                led.plot(wall_x, 1)
-                led.plot(wall_x, 2)
-                led.plot(wall_x, 3)
-                led.plot(wall_x, 4)
+                led.plotBrightness(wall_x, 0, wall_brightness)
+                led.plotBrightness(wall_x, 1, wall_brightness)
+                led.plotBrightness(wall_x, 2, wall_brightness)
+                led.plotBrightness(wall_x, 3, wall_brightness)
+                led.plotBrightness(wall_x, 4, wall_brightness)
+            }
+            if (wall_x == 0) {
+                led.plot(point_x, point_y)
             }
         } else {
         	
