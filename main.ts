35ecd8ad1f350644e147ff86ac369381157f0367
sherdoslav <seryji2010cz@gmail.com>
2025-03-26 21:34:09 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,18 +22,44 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+function light_the_wall () {
+    if (wall_hole != 0) {
+        led.plotBrightness(wall_x, 0, wall_brightness)
+    }
+    if (wall_hole != 1) {
+        led.plotBrightness(wall_x, 1, wall_brightness)
+    }
+    if (wall_hole != 2) {
+        led.plotBrightness(wall_x, 2, wall_brightness)
+    }
+    if (wall_hole != 3) {
+        led.plotBrightness(wall_x, 3, wall_brightness)
+    }
+    if (wall_hole != 4) {
+        led.plotBrightness(wall_x, 4, wall_brightness)
+    }
+}
 // on B button press
 input.onButtonPressed(Button.B, function () {
 	
 })
+function extinguish_the_wall () {
+    led.unplot(wall_x, 0)
+    led.unplot(wall_x, 1)
+    led.unplot(wall_x, 2)
+    led.unplot(wall_x, 3)
+    led.unplot(wall_x, 4)
+}
+let wall_hole = 0
 let can_start = 0
+let wall_brightness = 0
 let wall_x = 0
 let point_y = 0
 let point_x = 0
 point_x = 1
 point_y = 2
 wall_x = 4
-let wall_brightness = 100
+wall_brightness = 100
 basic.showLeds(`
     . . . . .
     . # . # .
@@ -51,42 +77,24 @@ basic.showLeds(`
     `)
 can_start = 1
 let wall_status = 1
+wall_hole = randint(0, 4)
 basic.forever(function () {
     basic.pause(500)
     if (can_start == 2) {
         if (wall_status == 1) {
-            led.plotBrightness(wall_x, 0, wall_brightness)
-            led.plotBrightness(wall_x, 1, wall_brightness)
-            led.plotBrightness(wall_x, 2, wall_brightness)
-            led.plotBrightness(wall_x, 3, wall_brightness)
-            led.plotBrightness(wall_x, 4, wall_brightness)
+            light_the_wall()
             wall_status = 2
         } else if (wall_status == 2) {
             if (wall_x != 0) {
-                led.unplot(wall_x, 0)
-                led.unplot(wall_x, 1)
-                led.unplot(wall_x, 2)
-                led.unplot(wall_x, 3)
-                led.unplot(wall_x, 4)
+                extinguish_the_wall()
                 wall_x += -1
-                led.plotBrightness(wall_x, 0, wall_brightness)
-                led.plotBrightness(wall_x, 1, wall_brightness)
-                led.plotBrightness(wall_x, 2, wall_brightness)
-                led.plotBrightness(wall_x, 3, wall_brightness)
-                led.plotBrightness(wall_x, 4, wall_brightness)
+                light_the_wall()
             } else {
-                led.unplot(wall_x, 0)
-                led.unplot(wall_x, 1)
-                led.unplot(wall_x, 2)
-                led.unplot(wall_x, 3)
-                led.unplot(wall_x, 4)
+                extinguish_the_wall()
                 wall_x = 4
+                wall_hole = randint(0, 4)
                 basic.pause(200)
-                led.plotBrightness(wall_x, 0, wall_brightness)
-                led.plotBrightness(wall_x, 1, wall_brightness)
-                led.plotBrightness(wall_x, 2, wall_brightness)
-                led.plotBrightness(wall_x, 3, wall_brightness)
-                led.plotBrightness(wall_x, 4, wall_brightness)
+                light_the_wall()
             }
             if (wall_x == 0) {
                 led.plot(point_x, point_y)
