fcd1e3be9fa1fb4cb13e5b2128363e89e1caa3ea
sherdoslav <seryji2010cz@gmail.com>
2025-03-26 15:09:49 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ input.onButtonPressed(Button.A, function () {
     } else if (can_start == 2) {
         led.unplot(point_x, point_y)
         if (point_y != 4) {
-        	
+            point_y += 1
         } else {
             point_y = 0
         }
@@ -48,6 +48,10 @@ basic.showLeds(`
 can_start = 1
 let wall_status = 1
 loops.everyInterval(500, function () {
+	
+})
+basic.forever(function () {
+    basic.pause(500)
     if (can_start == 2) {
         if (wall_status == 1) {
             led.plot(wall_x, 0)
@@ -57,22 +61,34 @@ loops.everyInterval(500, function () {
             led.plot(wall_x, 4)
             wall_status = 2
         } else if (wall_status == 2) {
-            led.unplot(wall_x, 0)
-            led.unplot(wall_x, 1)
-            led.unplot(wall_x, 2)
-            led.unplot(wall_x, 3)
-            led.unplot(wall_x, 4)
-            wall_x += -1
-            led.unplot(wall_x, 0)
-            led.unplot(wall_x, 1)
-            led.unplot(wall_x, 2)
-            led.unplot(wall_x, 3)
-            led.unplot(wall_x, 4)
+            if (wall_x != 0) {
+                led.unplot(wall_x, 0)
+                led.unplot(wall_x, 1)
+                led.unplot(wall_x, 2)
+                led.unplot(wall_x, 3)
+                led.unplot(wall_x, 4)
+                wall_x += -1
+                led.plot(wall_x, 0)
+                led.plot(wall_x, 1)
+                led.plot(wall_x, 2)
+                led.plot(wall_x, 3)
+                led.plot(wall_x, 4)
+            } else {
+                led.unplot(wall_x, 0)
+                led.unplot(wall_x, 1)
+                led.unplot(wall_x, 2)
+                led.unplot(wall_x, 3)
+                led.unplot(wall_x, 4)
+                wall_x = 4
+                basic.pause(200)
+                led.plot(wall_x, 0)
+                led.plot(wall_x, 1)
+                led.plot(wall_x, 2)
+                led.plot(wall_x, 3)
+                led.plot(wall_x, 4)
+            }
         } else {
         	
         }
     }
-})
-basic.forever(function () {
-	
 })
